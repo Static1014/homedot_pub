f82a4a0b9682ec5f4bc5428df44c0cf6ff68e52e
Static1014 <420048248@qq.com>
2023-10-14 23:56:46 +0800

    1. 图创主页、隐私政策页制作；

--- post_creator/index.js
+++ post_creator/index.js
@@ -9,18 +9,42 @@ $(function () {
     el: '#vue_id',
     components: {},
     data: {
-      name: "相思",
-      author: "王维",
+      store: {
+        // todo 下载地址
+        hw: '',
+        bd: '',
+        gp: '',
+      },
       footer: {
         author: "<span>主办单位：熊健&emsp;邮箱：<a href='mailto:420048248@qq.com'>420048248@qq.com</a></span>" +
-          "&emsp;<a id='ga' href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=12011102001541' target='_blank'><img src='../app_base/assets/img/ic_beian.png' alt='备案'/>津公网安备 12011102001541号</a>" +
+          "&emsp;<a id='ga' href='https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=12011102001541' target='_blank'><img src='../app_base/assets/img/ic_beian.png' alt='备案'/>津公网安备 12011102001541号</a>" +
           "&emsp;<a href='https://beian.miit.gov.cn' target='_blank'>津ICP备2023002266号-1</a>"
+      },
+      isEn: false,
+    },
+    computed: {
+      name() {
+        return this.isEn ? 'Post Creator' : '图创'
       }
     },
-    computed: {},
-    methods: {},
+    methods: {
+      clickDownload(index) {
+        switch (index) {
+          case 0:
+            // google play
+            break
+          case 1:
+            // 百度网盘
+            break
+          case 2:
+            // 华为
+            break
+        }
+      }
+    },
     mounted() {
       hideLoading()
+      this.isEn = parseGetParam('lang') === 'en'
     }
   })
 })
