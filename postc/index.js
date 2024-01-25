38138a28e2623e792f9f27ca56584bd640e4c447
Static1014 <420048248@qq.com>
2024-01-25 17:26:50 +0800

    1. 新增应用宝下载源；

--- postc/index.js
+++ postc/index.js
@@ -17,7 +17,7 @@ $(function () {
           name: 'Google Play',
           nameEn: 'Google Play',
           isSvg: true,
-          img:'',
+          img: '',
           ok: true, // 是否审核通过
           url: 'https://play.google.com/store/apps/details?id=com.static4u.pic_maker' // 下载地址
         },
@@ -25,23 +25,31 @@ $(function () {
           visible: true,
           name: '百度网盘',
           nameEn: 'Baidu Cloud Disk',
-          img:'assets/img/store/bd_cloud.png',
+          img: 'assets/img/store/bd_cloud.png',
           ok: true,
           url: 'https://pan.baidu.com/s/1Gzry7b1JUgfLkFMQCkOxHw?pwd=tpr7'
         },
         {
           visible: true,
           name: '华为应用市场',
-          nameEn: 'Huawei App Store',
-          img:'assets/img/store/huawei.png',
+          nameEn: 'Huawei AppGallery',
+          img: 'assets/img/store/huawei.png',
           ok: false,
           url: ''
         },
         {
           visible: true,
+          name: '腾讯应用宝',
+          nameEn: 'Tencent App Gallery',
+          img: 'assets/img/store/yyb.svg',
+          ok: true,
+          url: 'https://sj.qq.com/appdetail/com.static4u.pic_maker'
+        },
+        {
+          visible: true,
           name: '123云盘',
           nameEn: '123 Cloud Disk',
-          img:'assets/img/store/123.ico',
+          img: 'assets/img/store/123.ico',
           ok: true,
           url: 'https://www.123pan.com/s/4CQmjv-4UdGA.html'
         },
@@ -49,7 +57,7 @@ $(function () {
           visible: false,
           name: '普通下载',
           nameEn: 'Web Download',
-          img:'assets/img/logo_192.png',
+          img: 'assets/img/logo_192.png',
           ok: true,
           // url: './apk/pc_1.1.1_3_23.10.27.apk'
           url: ''
